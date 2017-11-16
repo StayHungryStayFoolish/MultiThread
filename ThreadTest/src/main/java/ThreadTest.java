23bbf8e54d2f848bd317c7b01e1a3d1fbdd363b1
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-16 23:40:17 +0800

    commit

---
+++ ThreadTest/src/main/java/ThreadTest.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:37 on 17/11/16.
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool();
+    }
+}
