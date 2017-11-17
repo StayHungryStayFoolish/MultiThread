c2e5a56b412416007294614bb055ccad8f2b08e8
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-17 17:38:53 +0800

    commit

--- ThreadTest/src/main/java/JoinTest.java
+++ ThreadTest/src/main/java/JoinTest.java
@@ -11,6 +11,8 @@ public class JoinTest {
         JoinThread t2 = new JoinThread("t2");
         t1.start();
         t2.start();
+        // 如果没有 join，合并线程，主方法可能在最初/中间执行完成
+        // 如果此时主方法需要当前线程的数据时，是获取不到的。
         t1.join();
         t2.join();
         System.out.println("主线程开始执行");
@@ -28,7 +30,26 @@ public class JoinTest {
  * t2>>11执行了2次
  * t2>>11执行了3次
  * t2>>11执行了4次
+ * <p>
+ * t1和t2都执行完才继续主线程的执行，所谓合并，就是等待其它线程执行完，再执行当前线程，执行起来的效果就好像把其它线程合并到当前线程执行一样。
  */
+
+/**
+ * 去掉 join 后，主方法提前执行了
+ t1>>10执行了0次
+ t1>>10执行了1次
+ t1>>10执行了2次
+ t1>>10执行了3次
+ 主线程开始执行
+ t2>>11执行了0次
+ t1>>10执行了4次
+ t2>>11执行了1次
+ t2>>11执行了2次
+ t2>>11执行了3次
+ t2>>11执行了4次
+ */
+
+
 class JoinThread extends Thread {
     public JoinThread(String name) {
         super(name);
